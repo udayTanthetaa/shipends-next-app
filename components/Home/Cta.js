7d8f29170b0d782045221858bb816cf3ff088a4e
udayTanthetaa <uday@tanthetaa.co.in>
2023-01-20 16:24:26 +0530

    update

--- components/Home/Cta.js
+++ components/Home/Cta.js
@@ -2,20 +2,17 @@ import Socials from "./Socials";
 import Header from "./Header";
 
 const Cta = () => {
-    return (
-        <>
-            <div
-                className="flex flex-col items-center text-center min-h-screen w-full place-content-center
-                           bg-gradient-to-br from-black to-zeus"
-            >
-                <Header />
+	return (
+		<>
+			<div className="flex flex-col items-center w-full min-h-screen text-center place-content-center bg-gradient-to-br from-black to-zeus">
+				<Header />
 
-                <div className="mt-4 md:mt-6 flex flex-row space-x-6">
-                    <Socials />
-                </div>
-            </div>
-        </>
-    );
+				<div className="flex flex-row mt-4 space-x-6 md:mt-6">
+					<Socials />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Cta;
--- components/Home/Header.js
+++ components/Home/Header.js
@@ -13,14 +13,14 @@ const Header = () => {
 	return (
 		<>
 			{/* TAGLINE */}
-			<div className="flex flex-col p-3 font-black text-[40px] md:text-[80px] drop-shadow-lg -space-y-4 md:-space-y-10">
-				<div className="text-transparent bg-clip-text bg-gradient-to-r from-golden to-magenta">
+			<div className="flex flex-col -space-y-4 p-3 text-[40px] font-black drop-shadow-lg md:-space-y-10 md:text-[80px]">
+				<div className="text-transparent bg-gradient-to-r from-golden to-magenta bg-clip-text">
 					Build Cool Products
 				</div>
 				<div>
 					{/* <span className="italic text-grey">@</span>&nbsp; */}
 					<Neon>
-						<span className="italic text-transparent bg-clip-text bg-gradient-to-br from-bianca to-white mt-5">
+						<span className="mt-5 italic text-transparent bg-gradient-to-br from-bianca to-white bg-clip-text">
 							without reading docs
 						</span>
 					</Neon>
@@ -29,22 +29,18 @@ const Header = () => {
 			{/* TAGLINE */}
 
 			{/* DESCRIPTION */}
-			<div className="p-3 mt-2 md:mt-3 text-grey font-bold text-[25px] md:text-[40px] max-w-xl md:max-w-4xl leading-[30px] md:leading-[50px]">
+			<div className="mt-2 max-w-xl p-3 text-[25px] font-bold leading-[30px] text-grey md:mt-3 md:max-w-4xl md:text-[40px] md:leading-[50px]">
 				{/* We simplify Web3 docs that you can explain your mum + ship with
 				her together. */}
-				We go through Web3 docs, simplify them and create notes, so you
-				can start building <br />{" "}
+				We go through Web3 docs, simplify them and create notes, so you can start building <br />{" "}
 				<span className="italic text-amour">-- within 5 min.</span>
 			</div>
 			{/* DESCRIPTION */}
 
 			{/* GET STARTED */}
 			<Link href="/learn">
-				<div className="bg-gradient-to-r from-golden to-magenta py-1 px-3 md:py-2 md:px-4 rounded-xl mt-4 md:mt-6">
-					<div
-						className="text-transparent bg-clip-text bg-gradient-to-br from-bianca to-white font-extrabold
-			text-2xl md:text-4xl"
-					>
+				<div className="px-3 py-1 mt-4 rounded-xl bg-gradient-to-r from-golden to-magenta md:mt-6 md:py-2 md:px-4">
+					<div className="text-2xl font-extrabold text-transparent bg-gradient-to-br from-bianca to-white bg-clip-text md:text-4xl">
 						{/* Get Started */}
 						Lesgoo!
 					</div>
