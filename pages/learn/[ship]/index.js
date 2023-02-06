2ecead84cff475acfb51194f18fd95ece3c42051
udayTanthetaa <uday@tanthetaa.co.in>
2023-02-06 17:41:24 +0530

    added avatar

--- pages/learn/[ship]/index.js
+++ pages/learn/[ship]/index.js
@@ -3,6 +3,7 @@ import { Octokit } from "octokit";
 import { Constants } from "../../../components";
 import Link from "next/link";
 import Image from "next/image";
+import { BigHead } from "@bigheads/core";
 
 export const getStaticProps = async (context) => {
 	const octokit = new Octokit({
@@ -65,8 +66,8 @@ const Ship = ({ index }) => {
 							className="mt-[12px] blur-2xl absolute w-full object-cover object-center rounded-md md:rounded-lg lg:rounded-xl h-[150px] md:h-[200px] lg:h-[250px]"
 						/>
 						<div
-							className="absolute place-content-center h-[150px] md:h-[200px] lg:h-[250px] z-20 flex flex-col items-center w-full
-                        mt-[12px] text-5xl md:text-8xl lg:text-11xl font-black text-isWhite drop-shadow-md capitalize"
+							className="absolute place-content-center h-[150px] md:h-[200px] lg:h-[250px] z-[2] flex flex-col items-center w-full
+                        mt-[12px] text-5xl md:text-8xl lg:text-11xl font-black text-isWhite drop-shadow-lg capitalize"
 						>
 							{index.parent}
 						</div>
@@ -76,24 +77,61 @@ const Ship = ({ index }) => {
 						src={index.banner}
 						alt={`${index.parent} banner`}
 						layout={"fill"}
-						className="z-10 w-full object-cover object-center rounded-md md:rounded-lg lg:rounded-xl h-[150px] md:h-[200px] lg:h-[250px]"
+						className="z-[1] w-full object-cover object-center rounded-md md:rounded-lg lg:rounded-xl h-[150px] md:h-[200px] lg:h-[250px]
+						"
 					/>
 
 					{index.sections.map((section, key) => {
 						return (
-							<Link href={`${index.parent}/${index[section].path}`} key={key} passHref className="group">
+							<Link
+								href={`${index.parent}/${index[section].path}`}
+								key={key}
+								passHref
+								className="z-[3] group"
+							>
 								<div
-									className="w-full rounded-md bg-isWhite transition duration-300 ease-in-out delay-50 group-hover:bg-isBlueLightEmphasis md:rounded-lg lg:rounded-xl
-                                shadow-sm flex flex-col p-[15px] md:p-[20px] lg:p-[25px]"
+									className="w-full rounded-md bg-isWhite transition duration-300 ease-in-out delay-50 group-hover:bg-isBlueDark md:rounded-lg lg:rounded-xl
+                                shadow-sm flex flex-col p-[15px] md:p-[20px] lg:p-[25px] "
 								>
 									<div className="text-lg font-bold leading-3 transition duration-300 ease-in-out group-hover:text-isWhite group-hover:text-is md:text-xl lg:text-2xl text-isGrayDarkEmphasis4 delay-50">
 										{index[section].title}
 									</div>
 									<div
-										className="mt-[2px] text-xs font-medium md:text-sm lg:text-md text-isGrayDark2
-                                     group-hover:text-isWhite transition duration-300 ease-in-out delay-50"
+										className="mt-[4px] text-xs font-medium md:text-sm lg:text-md text-isGrayDark2
+                                     group-hover:text-isWhite transition duration-300 ease-in-out delay-50 flex flex-row items-center"
 									>
-										100+ Shippers . 904 Likes . 5 min
+										<div className="flex flex-row items-center -mt-[8px] ">
+											<BigHead className="z-50 w-4 h-4 p-0 md:h-6 md:w-6 lg:h-8 lg:w-8 drop-shadow-lg " />
+											<BigHead className="z-40 lg:-ml-[14px] md:-ml-[12px] -ml-[10px] w-4 h-4 p-0 md:h-6 md:w-6 lg:h-8 lg:w-8 drop-shadow-lg" />
+											<BigHead className="z-30 lg:-ml-[14px] md:-ml-[12px] -ml-[10px] w-4 h-4 p-0 md:h-6 md:w-6 lg:h-8 lg:w-8 drop-shadow-lg" />
+											<BigHead className="z-20 lg:-ml-[14px] md:-ml-[12px] -ml-[10px] w-4 h-4 p-0 md:h-6 md:w-6 lg:h-8 lg:w-8 drop-shadow-lg" />
+											<BigHead className="z-10 lg:-ml-[14px] md:-ml-[12px] -ml-[10px] w-4 h-4 p-0 md:h-6 md:w-6 lg:h-8 lg:w-8 drop-shadow-lg" />
+										</div>
+										&nbsp;
+										{Math.floor(Math.random() * 100 + 100)}+ Shippers &nbsp;--&nbsp;
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 24 24"
+											className="w-3 h-3 transition duration-300 ease-in-out md:h-4 md:w-4 lg:h-5 lg:w-5 fill-isRedLight group-hover:fill-isRedDark stroke-none delay-50"
+										>
+											<path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+										</svg>
+										&nbsp;
+										{Math.floor(Math.random() * 100 + 100)}
+										&nbsp;--&nbsp;
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 24 24"
+											className="w-3 h-3 transition duration-300 ease-in-out md:h-4 md:w-4 lg:h-5 lg:w-5 fill-isGreenLight group-hover:fill-isGreenDark stroke-none delay-50"
+										>
+											<path
+												fillRule="evenodd"
+												d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z"
+												clipRule="evenodd"
+											/>
+										</svg>
+										&nbsp;
+										{index[section].takes} min
 									</div>
 									<hr className="mt-[4px] w-full transition duration-300 ease-in-out delay-50 border-isGrayLightEmphasis4 group-hover:border-isWhite" />
 									<div
