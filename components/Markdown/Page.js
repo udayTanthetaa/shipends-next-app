20918befd40a2bdc54fcea07f531357569f29616
udayTanthetaa <uday@tanthetaa.co.in>
2023-02-15 17:19:02 +0530

    added framer

--- components/Markdown/Page.js
+++ components/Markdown/Page.js
@@ -1,8 +1,30 @@
 import NavigationPanel from "./NavigationPanel";
 import SectionPanel from "./SectionPanel";
 import Content from "./Content";
+import { motion } from "framer-motion";
 
 const Page = ({ frontmatter, source, index }) => {
+	const container = {
+		hidden: { opacity: 1, scale: 0 },
+
+		visible: {
+			opacity: 1,
+			scale: 1,
+			transition: {
+				delayChildren: 0.2,
+				staggerChildren: 0.2,
+			},
+		},
+	};
+
+	const item = {
+		hidden: { y: 20, opacity: 0 },
+		visible: {
+			y: 0,
+			opacity: 1,
+		},
+	};
+
 	return (
 		<>
 			<div className="flex flex-col items-center w-full min-h-screen bg-isGrayLightEmphasis6 place-content-start">
