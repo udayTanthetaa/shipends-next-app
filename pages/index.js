97fcae259eb913e436f4a5bbec73d3f544b79090
Uday Tanthetaa <uday@tanthetaa.co.in>
2023-03-03 17:37:59 +0530

    home update

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,28 @@
 import Head from "next/head";
 import Image from "next/image";
 
-import { Cta } from "../components";
-import { Button } from "ui";
+import { Button, InvisibleNavbar } from "ui";
 
 const Home = () => {
 	return (
 		<>
 			<Head>
 				<title>Shipends</title>
-				<meta
-					name="description"
-					content="Ship cool products, without reading docs."
-				/>
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1"
-				/>
+				<meta name="description" content="Ship cool products, without reading docs." />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className="flex flex-col w-full min-h-screen bg-isGrayLightEmphasis6 place-content-center items-center"></main>
+			<main className="flex flex-col items-center w-full min-h-screen bg-isGrayLightEmphasis6 place-content-start">
+				<InvisibleNavbar />
+
+				<div className="flex flex-col items-center w-full max-w-6xl mt-3">
+					<div className="flex flex-col items-center w-full min-h-[60vh] place-content-center bg-isWhite rounded-2xl shadow-sm shadow-isGrayLightEmphasis3">
+						<div>build cool products</div>
+						<div>without reading docs</div>
+					</div>
+				</div>
+			</main>
 		</>
 	);
 };
